2b85e28d101b7ea9845d55499633eb9de199887a
SANDAM22 <122832454+SANDAM22@users.noreply.github.com>
2023-03-20 21:48:51 -0300

    Terminado el index y también aproveché para corregir algunos errores al pasar la pestaña login de html a ejs

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,13 @@
+const path = require("path")
+const fs = require('fs');
 const controller = {
     index: (req, res) => {
-        return res.render('index')
+        const products = JSON.parse(fs.readFileSync(productsFilePath,"utf-8"));
+        return res.render('index', {products})
     }
 }
+
+const productsFilePath = path.join(__dirname,"../dataBase/products.json");
+
 
 module.exports = controller;
