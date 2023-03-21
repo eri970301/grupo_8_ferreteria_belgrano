3e697b1aa03e6dab6db244635c564e095b5941be
NicolasGalvarini57 <nicolas.galvarini157@gmail.com>
2023-03-20 23:23:21 -0300

    nuevo register actualizado

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,15 +5,10 @@ const controller = {
         const products = JSON.parse(fs.readFileSync(productsFilePath,"utf-8"));
         return res.render('index', {products})
     }
-<<<<<<< HEAD
   
     
 };
-=======
-}
-
 const productsFilePath = path.join(__dirname,"../dataBase/products.json");
 
 
->>>>>>> 4572438bf65f444e8d364218a63112b90c917221
 module.exports = controller;
