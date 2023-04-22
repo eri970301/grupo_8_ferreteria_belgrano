beac81a66b35ae5df77ae85bc4d48388e0e09a62
SANDAM22 <122832454+SANDAM22@users.noreply.github.com>
2023-04-22 17:40:02 -0300

    Corregidos graves errores en el css del register y avnzada la funcion de registrar un usuario

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,57 +1,58 @@
 const path = require("path")
 const fs = require('fs');
-const productsFilePath = path.join(__dirname,"../dataBase/products.json");
+const productsFilePath = path.join(__dirname, "../dataBase/products.json");
 const controller = {
     // Show all products
     index: (req, res) => {
-        const products = JSON.parse(fs.readFileSync(productsFilePath,"utf-8"));
-        return res.render('products', {products})
+        const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+        return res.render('products', { products })
     },
-    create: (req, res) =>{
+    create: (req, res) => {
         res.render('product-create-form')
     },
     store: (req, res) => {
-        const products = JSON.parse(fs.readFileSync(productsFilePath,"utf-8"));
+        const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
         let productoNuevo = {
-			id: products[products.length - 1].id + 1,
-			name: req.body.name,
-			price: parseInt(req.body.price),
-			discount: parseInt(req.body.discount),
-			category: req.body.category,
-			description: req.body.description,
-			image: req.file ? req.file.filename : 'default-image.png'
-		};
+            id: products[products.length - 1].id + 1,
+            name: req.body.name,
+            price: parseInt(req.body.price),
+            discount: parseInt(req.body.discount),
+            category: req.body.category,
+            description: req.body.description,
+            image: req.file ? req.file.filename : 'default-image.png'
+        };
         products.push(productoNuevo);
         let productsJSON = JSON.stringify(products, null, '');
         fs.writeFileSync(productsFilePath, productsJSON);
         res.redirect('/products')
     },
-    edit: (req, res) =>{
+    edit: (req, res) => {
         res.render('product-edit-form')
     },
-    cart: (req, res)=>{
+    cart: (req, res) => {
         res.render('productCart')
     },
-    delete: (req, res) =>{
+    delete: (req, res) => {
         let id = req.params.id;
         const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-        let finalProducts = products.filter(product =>{
+        let finalProducts = products.filter(product => {
             return product.id != id
         })
         let productsJSON = JSON.stringify(finalProducts, null, ' ');
         fs.writeFileSync(productsFilePath, productsJSON);
         res.redirect('/products');
     },
-    detail: (req,res) => {
+    detail: (req, res) => {
         const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-		let id = req.params.id
-		let product = products.find(product => product.id == id)
+        let id = req.params.id
+        let product = products.find(product => product.id == id)
 
-		res.render('/detail', {
-			product})
+        res.render('/detail', {
+            product
+        })
     }
-    
+
 }
 
 module.exports = controller
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,12 @@
  let express = require('express');
+const users = require('../controllers/usersController');
 let router = express.Router();
 const usersController = require('../controllers/usersController')
 
 router.get('/login', usersController.login);   
 //ruta de tipo get /usuarios/registro
-router.get('/register', usersController.registro);  
+router.get('/register', usersController.registro);
+router.post('/register',usersController.guardarUsuario)  
 
 router.get('/personal', usersController.personal);  
 
