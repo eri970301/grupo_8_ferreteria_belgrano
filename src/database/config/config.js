c4dfa8018713aa956e3796448e92d328e307fac8
erika970301@live.com <erika970301@live.com>
2023-06-17 20:36:32 -0500

    Imagen de usuario arreglada

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,18 +2,21 @@ module.exports = {
   "development": {
     "username": "root",
     "database": "mydb",
+    "password": "12345",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "database": "mydb",
+    "password": "12345",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "database": "mydb",
+    "password": "12345",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
