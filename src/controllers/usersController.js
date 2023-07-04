7e020e17df44de4d6b7fbf089641dc8d038a6166
NicolasGalvarini57 <nicolas.galvarini157@gmail.com>
2023-07-04 00:13:33 -0300

    solo admi puede editar y eliminar

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -8,130 +8,145 @@ const db = require('../database/models');
 
 
 const users = {
-    login: (req, res) => {
-         return res.render('users/login') 
-    },
+  login: (req, res) => {
+    return res.render('users/login')
+  },
 
-    
-    processLogin: (req, res) => {
-      const { email, password } = req.body;
-    
-      db.Users.findOne({ where: { email } })
-        .then(users => {
-          if (!users) {
-            
-             return res.render('users/login'); 
+
+  processLogin: (req, res) => {
+
+    const { email, password } = req.body;
+
+    db.Users.findOne({ where: { email } })
+      .then(users => {
+        if (!users) {
+
+          return res.render('users/login');
+        }
+
+        if (bcrypt.compareSync(password, users.password)) {
+          if (users.type === "Administrador") {
+
+            res.cookie('admi', users.email, { maxAge: 60000 });
+            db.Products.findAll()
+              .then((product) => {
+                return res.render('Administrador', { products: product })
+              });
+          } else {
+            res.cookie('cliente', users.email, { maxAge: 60000 });
+            db.Products.findAll()
+              .then((product) => {
+                return res.render('Cliente', { products: product })
+              })
           }
-    
-          if (bcrypt.compareSync(password, users.password)) {
-              res.cookie('recordame', users.email, { maxAge: 60000 });
-              return res.render('users/Cliente', {users : [users] });
-            
-          }
-    
-        
-          return res.render('users/login'); 
+        } else {
+
+          return res.render('users/login');
+        }
+      })
+      .catch(error => {
+        console.error('Error en processLogin:', error);
+        return res.status(500).send('Error en el servidor');
+      });
+  },
+
+
+  registro: (req, res) => {
+    return res.render('users/register')
+  },
+
+
+
+  guardarUsuario: (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      const users = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10),
+        type: req.body.role,
+        image: req.file ? req.file.filename : 'user.jpg'
+      };
+      db.Users.create(users)
+        .then(user => {
+          res.redirect("/users/personal")
+          console.log(user)
         })
         .catch(error => {
-          console.error('Error en processLogin:', error);
+          console.error('Error en guardarUsuario:', error);
           return res.status(500).send('Error en el servidor');
         });
-    },
-    
-    
-    registro: (req, res) => {
-        return res.render('users/register')
-    },
+    } else {
+      res.render('users/register', {
+        errors: errors.array(),
+        old: req.body
+      });
+    }
+  },
 
+  personal: (req, res) => {
+    db.Users.findAll()
 
+      .then(users => {
 
-    guardarUsuario: (req, res) => {
-        const errors = validationResult(req);
-        if (errors.isEmpty()) {
-          const users = {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, 10),
-            type: req.body.role,
-            image: req.file ? req.file.filename : 'user.jpg'
-          };
-          db.Users.create(users)
-            .then(user => {
-              res.redirect("/users/personal")
-              console.log(user)
-            })
-            .catch(error => {
-              console.error('Error en guardarUsuario:', error);
-              return res.status(500).send('Error en el servidor');
-            });
-        } else {
-          res.render('users/register', {
-            errors: errors.array(),
-            old: req.body
-          });
-        }
-      },
-      
-    personal: (req, res) => {
-              db.Users.findAll()
-    
-          .then(users => {
-           
-            return res.render('users/personal', { users: users });
-          })
-          .catch(error => {
-            console.error('Error en personal:', error);
-            return res.status(500).send('Error en el servidor');
-          });
-      },
-      
-
-   Eliminar: (req, res) => {
-      let id = req.params.id;
-      db.Users.destroy({
-          where: {
-            
-              iduser: id
-          }
+        return res.render('users/personal', { users: users });
       })
-          .then(() => {
-              res.redirect('/');
-          })
-          .catch(error => {
-              console.error('Error al eliminar el Usuario:', error);
-              res.redirect('/');
-          })
-          },
-          
-    edit:(req, res) => {
-      let id = req.params.id;
-      db.Users.findByPk(id)
-          .then(user => {
-            return res.render('users/Edit', { user: user });
-            console.log(users)
-          })
-          .catch(error => {
-              console.error('Error al obtener el detalle del producto:', error);
-              res.redirect('/products');
-          });
+      .catch(error => {
+        console.error('Error en personal:', error);
+        return res.status(500).send('Error en el servidor');
+      });
+  },
 
 
+  Eliminar: (req, res) => {
+    let id = req.params.id;
+    db.Users.destroy({
+      where: {
 
-    }, 
-    detail: (req, res) => {
-      let id = req.params.id;
-            db.Users.findByPk(id)
-                .then(user => {
-                  return res.render('users/delet', { user: user });
-                  console.log(users)
-                })
-                .catch(error => {
-                    console.error('Error al obtener el detalle del producto:', error);
-                    res.redirect('/products');
-                });
+        iduser: id
+      }
+    })
+      .then(() => {
+        res.redirect('/');
+      })
+      .catch(error => {
+        console.error('Error al eliminar el Usuario:', error);
+        res.redirect('/');
+      })
+  },
+
+  edit: (req, res) => {
+    let id = req.params.id;
+    db.Users.findByPk(id)
+      .then(user => {
+        return res.render('users/Edit', { user: user });
+        console.log(users)
+      })
+      .catch(error => {
+        console.error('Error al obtener el detalle del producto:', error);
+        res.redirect('/products');
+      });
+
+  },
+  detail: (req, res) => {
+    let id = req.params.id;
+    db.Users.findByPk(id)
+      .then(user => {
+        if (req.cookies && req.cookies.admi) {
+          return res.render('users/delet', { user: user });
+        } else {
+          return res.render('users/deletClient', { user: user });
         }
-    
-    };
 
+      })
+      .catch(error => {
+        console.error('Error al obtener el detalle del producto:', error);
+        res.redirect('/products');
+      });
+
+
+  }
+
+};
 module.exports = users
--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "database": "mydb",
+    "database": "ferreteriabelgrano",
     "password": null,
     "host": "127.0.0.1",
     "dialect": "mysql"
